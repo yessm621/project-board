9c96a236bd3e3c762af43d1a4c53994caf60d532
yessm621 <yessm621@gmail.com>
2023-05-13 01:24:27 +0900

    #22 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고 연관된 DTO 를 재설계함.
    테스트가 필요로 하는 repository 쿼리 메소드도 작성.

--- src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleDto.java
@@ -1,14 +1,43 @@
 package com.fastcampus.projectboard.dto;
 
+import com.fastcampus.projectboard.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
+        Long id,
+        UserAccountDto userAccountDto,
         String title,
         String content,
         String hashtag,
-        LocalDateTime createdDate,
-        String createdBy) {
-    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createdDate, String createdBy) {
-        return new ArticleDto(title, content, hashtag, createdDate, createdBy);
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy) {
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
     }
 }
--- src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -21,4 +21,13 @@ public class ArticleCommentService {
     public List<ArticleCommentDto> searchArticleComments(long articleId) {
         return List.of();
     }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
 }
