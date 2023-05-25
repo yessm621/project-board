05c2ac4e34632dc94a08c812afb06d9004138f83
yessm621 <yessm621@gmail.com>
2023-05-25 23:19:13 +0900

    #34 - 게시판 페이지 검색바 기능 구현 + 페이징, 정렬과 연동

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('yessm621', 'asdf1234', 'yessm621', 'yessm621@mail.com', 'I am yessm.', now(), 'yessm621', now(), 'yessm621');
+    ('yessm621', 'asdf1234', 'yessm621', 'yessm621@email.com', 'I am yessm.', now(), 'yessm621', now(), 'yessm621');
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('yessm6212', 'asdf1234', 'yessm6212', 'yessm6212@mail.com', 'I am yessm2.', now(), 'yessm6212', now(), 'yessm6212');
+    ('yessm6212', 'asdf1234', 'yessm6212', 'yessm6212@email.com', 'I am yessm2.', now(), 'yessm6212', now(), 'yessm6212');
 
 
 -- 게시글
